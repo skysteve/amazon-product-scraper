09566982056932fa37faaa4f42b81f404e88f9e1
Steve Jenkins <steve@skysteve.com>
2018-10-07 21:14:47 +0100

    basics of getting data from the page

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import { scrapeProductData } from "./puppeteer_scripts/scrapeProductData";
+
+scrapeProductData('B002QYW8LW')
+  .then((productData) => {
+    console.log(JSON.stringify(productData, null, 2));
+  })
+  .catch((ex) => {
+    console.error(ex);
+  });
---
+++ src/puppeteer_scripts/scrapeProductData.ts
@@ -0,0 +1,54 @@
+import puppeteer from 'puppeteer';
+
+const selectors = {
+  category: '#wayfinding-breadcrumbs_feature_div ul',
+  dimensions: '.size-weight', // note - we want the 2nd of these
+  title: '#productTitle',
+  rank: '#SalesRank .value'
+}
+const WAIT_TIMEOUT = 5; // seconds
+
+
+// TODO - not any
+function scrapeData(selectorList: any): any {
+  const result: any = {};
+
+  const elTitle = document.querySelector(selectorList.title);
+  const elCategory = document.querySelector(selectorList.category);
+  const elRank = document.querySelector(selectorList.rank);
+  const elDimensions = document.querySelectorAll(selectorList.dimensions);
+
+  // this shouldn't happen, but just incase, check all our elements exist
+  if (!elTitle || !elCategory || !elRank || !elDimensions || elDimensions.length < 1) {
+    throw new Error(`Could not find selector for ${selectorList.title}`);
+  }
+
+  // get all the data from the page
+  result.title = elTitle.textContent.trim();
+  result.category = elCategory.textContent.trim(); // TODO - needs tiding up
+  result.rank = elRank.textContent.trim(); // TODO - needs tiding up
+  result.dimensions = elDimensions[1].textContent.trim(); // TODO - needs tiding up
+
+  return result;
+}
+
+
+export async function scrapeProductData(asin: string) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(`https://www.amazon.com/dp/${asin}`);
+
+  // wait for the selectors to appear on the page
+  await Promise.all(Object.values(selectors).map((selector) => {
+    return page.waitForSelector(selector, {
+      timeout: WAIT_TIMEOUT * 1000
+    });
+  }));
+
+  // get the data from the page
+  const jsonData = await page.evaluate(scrapeData, selectors);
+
+  // close the page, and return the result
+  await browser.close();
+  return jsonData;
+}
